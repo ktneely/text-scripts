c218b5fb9514b22324705b45e8240958bca97af0
Kevin Neely <ktneely@purestorage.com>
2023-07-12 15:31:19 -0700

    extract news articles from RSSguard db to txt or md

---
+++ rssfeeds2txt.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+
+
+
+import argparse
+import sqlite3
+import html2text
+from datetime import datetime
+import os.path
+
+def convert_to_iso8601(timestamp):
+    return datetime.utcfromtimestamp(timestamp / 1000).isoformat()
+
+def convert_html_to_markdown(html):
+    h = html2text.HTML2Text()
+    h.body_width = 0
+    return h.handle(html)
+
+
+def convert_html_to_text(html):
+    h = html2text.HTML2Text()
+    h.body_width = 0
+    h.ignore_links = True
+    h.ignore_images = True
+    h.ignore_tables = True
+    h.ignore_emphasis = True
+    return h.handle(html)
+
+def extract_messages_from_database(database_path):
+    try:
+        conn = sqlite3.connect(database_path)
+        c = conn.cursor()
+        c.execute("SELECT id, title, date_created, url, contents, author FROM Messages")
+        messages = c.fetchall()
+        conn.close()
+        return messages
+    except sqlite3.Error as e:
+        print("Error connecting to the database:", e)
+        return []
+
+def create_text_documents(messages, output_path, output_format):
+    for message in messages:
+        message_id = message[0]
+        title = message[1]
+        date_created = message[2]
+        url = message[3]
+        contents = message[4]
+        author = message[5]
+
+        if output_format == "markdown":
+            filename = os.path.join(output_path, f"rssarticle-{message_id}.md")
+        else:
+            filename = os.path.join(output_path, f"rssarticle-{message_id}.txt")
+        if os.path.exists(filename):
+            print(f"Skipping existing file: {filename}")
+            continue
+
+        with open(filename, "w") as file:
+            if output_format == "markdown":
+                file.write("---\n")
+                file.write(f"title: \"{title}\"\n")
+                file.write(f"date_created: {convert_to_iso8601(date_created)}\n")
+                file.write(f"author: \"{author}\"\n")
+                file.write("tags: #rss\n")
+                file.write("---\n\n")
+                file.write(f"# {title}\n\n")
+                file.write(convert_html_to_markdown(contents))
+            else:
+                file.write(f"{title}\n")
+                file.write(url + "\n\n")
+                file.write(convert_html_to_text(contents))
+
+        print(f"Created file: {filename}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract messages from a SQLite database and create text documents.")
+    parser.add_argument("database_file", help="Path to the SQLite database file")
+    parser.add_argument("output_path", help="Path to the output directory")
+    parser.add_argument("-f", "--format", choices=["markdown", "plaintext"], default="plaintext",
+                        help="Output format for the contents field: 'markdown' or 'plaintext' (default: plaintext)")
+
+    args = parser.parse_args()
+    database_file = args.database_file
+    output_path = args.output_path
+    output_format = args.format
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    messages = extract_messages_from_database(database_file)
+    create_text_documents(messages, output_path, output_format)
