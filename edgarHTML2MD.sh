77a2f588d4ef1086c877bdcb2c077b691c8e3255
Kevin Neely <ktneely@purestorage.com>
2024-02-26 12:13:30 -0800

    cutting off a couple extra leading lines

--- edgarHTML2MD.sh
+++ edgarHTML2MD.sh
@@ -4,6 +4,8 @@
 # to simplified Markdown files in the filings/md directory
 # Works on the massively over-formatted EDGAR docs from the SEC
 # but should work on other HTML files, as well
+#
+# This drops the first 4 lines, as they are often complete trash
 
 for file in filings/html/*
 do
@@ -11,6 +13,6 @@ do
     filename=$(basename "$file" .html)
     if [ ! -s filings/md/$filename.md ]; then
         echo "Now converting $filename ..."
-	pandoc -f html -t markdown_github-raw_html $file |tail -n +2 > filings/md/$filename.md
+	pandoc -f html -t markdown_github-raw_html $file |tail -n +4 > filings/md/$filename.md
     fi
 done
